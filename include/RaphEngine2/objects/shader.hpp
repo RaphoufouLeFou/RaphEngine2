c18ee11311cdcb75548c043cb5d5649d660e3286
RaphoufouLeFou <raphael.clerault@gmail.com>
2026-03-03 12:21:46 +0100

    implemented shader class

--- include/RaphEngine2/objects/shader.hpp
+++ include/RaphEngine2/objects/shader.hpp
@@ -15,30 +15,30 @@ namespace raphEngine::objects
 class RAPHENGINE_API Shader
 {
 public:
-    unsigned int ID;
-    Shader(const char* vShaderCode, const char* fShaderCode, const char* gShaderCode = nullptr);
-    static void Prepare();
+    Shader(const std::string& vShaderCode, const std::string& fShaderCode, const std::string& gShaderCode = "");
     void use();
-    void setBool(const char* name, bool value) const;
-    void setInt(const char* name, int value) const;
-    void setIntArray(const char* name, int count, int *value) const;
-    void setFloat(const char* name, float value) const;
-    void setFloatArray(const char* name, int count, float *value) const;
-    void setVec2(const char* name, const glm::vec2 value) const;
-    void setVec2(const char* name, float x, float y) const;
-    void setVec2Array(const char* name, int count, const glm::vec2* value) const;
-    void setVec3(const char* name, const glm::vec3 value) const;
-    void setVec3Array(const char* name, int count, const glm::vec3 *value) const;
-    void setVec3(const char* name, float x, float y, float z) const;
-    void setVec4(const char* name, const glm::vec4& value) const;
-    void setVec4(const char* name, float x, float y, float z, float w);
-    void setMat2(const char* name, const glm::mat2& mat) const;
-    void setMat3(const char* name, const glm::mat3& mat) const;
-    void setMat4(const char* name, const glm::mat4& mat) const;
+    void setBool(const std::string& name, bool value) const;
+    void setInt(const std::string& name, int value) const;
+    void setIntArray(const std::string& name, int count, int *value) const;
+    void setFloat(const std::string& name, float value) const;
+    void setFloatArray(const std::string& name, int count, float *value) const;
+    void setVec2(const std::string& name, const glm::vec2& value) const;
+    void setVec2(const std::string& name, float x, float y) const;
+    void setVec2Array(const std::string& name, int count, const glm::vec2* value) const;
+    void setVec3(const std::string& name, const glm::vec3& value) const;
+    void setVec3Array(const std::string& name, int count, const glm::vec3 *value) const;
+    void setVec3(const std::string& name, float x, float y, float z) const;
+    void setVec4(const std::string& name, const glm::vec4& value) const;
+    void setVec4(const std::string& name, float x, float y, float z, float w);
+    void setMat2(const std::string& name, const glm::mat2& mat) const;
+    void setMat3(const std::string& name, const glm::mat3& mat) const;
+    void setMat4(const std::string& name, const glm::mat4& mat) const;
     void setModel(const glm::mat4& mat, int index) const;
-    static std::vector<Shader*> LoadedShaders;
 
 private:
-    void checkCompileErrors(unsigned int shader, std::string type);
+
+    static std::vector<Shader*> loadedShaders_;
+    unsigned int id_;
+    void checkCompileErrors(unsigned int shader, const std::string& type);
 };
 }

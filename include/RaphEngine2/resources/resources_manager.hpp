18d5124c609269cda6fa7b54f6201c8c8cc39f7e
RaphoufouLeFou <raphael.clerault@gmail.com>
2026-08-07 23:47:06 +0200

    changed TODO for ressource system

--- include/RaphEngine2/resources/resources_manager.hpp
+++ include/RaphEngine2/resources/resources_manager.hpp
@@ -18,11 +18,9 @@ namespace raphEngine::resources
     class RAPHENGINE_API ResourcesManager
     {
     public:
-        // returns nullptr and print an message on std::cerr in case of a falure
         template <IsResource T>
         static Resource* load_shared_resource(const std::string& path);
 
-        // returns nullptr and print an message on std::cerr in case of a falure
         template <IsResource T>
         static std::unique_ptr<Resource>
         load_unique_resource(const std::string& path);
